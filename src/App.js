1cceed4470d1d84bfcda02050c3a66f0d0db4949
robyndalgleish <robyn.dalgleish@groupbyinc.com>
2019-06-06 13:15:26 -0400

    feat(demo): jibberish

--- src/App.js
+++ src/App.js
@@ -21,24 +21,24 @@ function App() {
             hook project up to circleci
             make sure you have a .circleci/config.yml file in root directory
               example:
-          version: 2
-          jobs:
-            release:
-              docker:
-                - image: circleci/node:8
-              steps:
-                - checkout
-                - run: npm install
-                # Run optional required steps before releasing
-                # - run: npm run build-script
-                - run: npx semantic-release
-
-          workflows:
-            version: 2
-            test_and_release:
-              # Run the test jobs first, then the release only when all the test jobs are successful
+              version: 2
               jobs:
-                - release
+                release:
+                  docker:
+                    - image: circleci/node:8
+                  steps:
+                    - checkout
+                    - run: npm install
+                    # Run optional required steps before releasing
+                    # - run: npm run build-script
+                    - run: npx semantic-release
+
+              workflows:ksjdbnvlakdfb
+                version: 2
+                test_and_release:
+                  # Run the test jobs first, then the release only when all the test jobs are successful
+                  jobs:
+                    - release
 
           Add GH_TOKEN GIT_CREDENTIALS and NPM_TOKENin circleci project settings
         
